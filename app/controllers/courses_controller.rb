e078844557713ca904c3301eea33afba5e8650e4
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-02-10 17:56:21 -0500

    added before_action for finding course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,11 @@
 class CoursesController < ApplicationController 
+  before_action :find_course, only: [:show, :edit, :update, :destroy]
 
   def index 
     @courses = Course.all 
   end 
 
   def show 
-    @course = Course.find(params[:id])
   end 
 
   def new 
@@ -25,12 +25,21 @@ class CoursesController < ApplicationController
   end 
   
   def update 
+    if @course.update(course_params)
+      redirect_to @course
+    else
+      render :edit
+    end
   end 
 
   def destroy 
   end 
 
   private 
+
+  def find_course 
+    @course = Course.find(params[:id])
+  end 
 
   def course_params 
     params.require(:course).permit(:name,:subject,:teacher)
