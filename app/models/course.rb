474be66bb61f4e3c0993f16d137b6e5e8aaac02f
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-02-10 20:18:37 -0500

    added in valdiations and displaying errors for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   has_many :notes 
   has_many :users, through: :notes 
+
+  validates :name, :subject, :course, presence: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :courses, through: :notes 
 
   has_secure_password 
+
+  validates :first_name, presence: true
+  validates :email, uniqueness: true
 end
