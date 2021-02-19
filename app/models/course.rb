3ce2a7006b7027c4cb631390e786d5187dba38d0
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-02-19 12:15:43 -0500

    added before_save capitalize method

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,15 @@
 class Course < ApplicationRecord
+  before_save :capitalize_name
+  
   has_many :notes
   has_many :users, through: :notes 
 
   validates :name, :subject, :teacher, presence: true
 
+  def capitalize_name
+    self.name = self.name.split.collect(&:capitalize).join(' ') 
+  end
+  
 
   def self.alphabetical_name
      order(name: :asc).to_a
